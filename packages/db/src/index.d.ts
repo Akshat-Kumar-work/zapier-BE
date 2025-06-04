0e7bfff8f796ec757e3ae1e6882b2986e5a28fc9
Akshat <iamakku0.0.0.1@gmail.com>
2025-06-04 10:48:36 +0530

    updates

--- packages/db/src/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare const prismaClientSingleton: () => any;
-declare global {
-    var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
-}
-declare const prisma: ReturnType<typeof prismaClientSingleton>;
-export default prisma;
-//# sourceMappingURL=index.d.ts.map
