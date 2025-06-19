4e5a6f786470ba9acf08a95a62fdfa7b266407c4
Akshat <iamakku0.0.0.1@gmail.com>
2025-06-19 23:56:15 +0530

    prisma : type fix

--- apps/hooks/src/index.ts
+++ apps/hooks/src/index.ts
@@ -1,16 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import beCommon from "@repo/be_common";
-import db from "@repo/db";
 import { Request,Response } from "express";
+import db from "@repo/db"
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
- 
 app.use(express.json());
+
+console.log(db)
 
 app.get("/", (req:Request, res:Response) => {
   return res.status(200).json(beCommon.success(null, "working fine"));
